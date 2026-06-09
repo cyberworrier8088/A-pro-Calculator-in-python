b2b12433ffcdffcf74bc51da9ab62233282f1f3d
cyberworrier8088 <cyberworrier39@gmail.com>
2026-06-09 15:46:12 +0530

    added plus and improved

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import banner
+import math
 
 banner.banner()
 
-option = input("Enter ur option: ")
+option = input("Enter ur input: ")
+
+num1 = input("Enter ur first num: ")
+num2 = input("Enter ur second num: ")
+num1 = int(num1)
+num2 = int(num2)
+
+if option == "1":
+    result = math.plus(num1, num2)
+    print(f"ur result: ", result)
