261ac0f230b464cbbc69e22f05c8e0362ef294a8
cyberworrier8088 <cyberworrier39@gmail.com>
2026-06-09 16:19:21 +0530

    full completed webt a small polish

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import math
 
 banner.banner()
 
-option = input("Enter ur input: ")
+option = input("Enter ur option: ")
 
 num1 = input("Enter ur first num: ")
 num2 = input("Enter ur second num: ")
@@ -17,5 +17,20 @@ elif option == "2":
     result = math.minus(num1, num2)
     print(f"ur result: ", result)
 elif option == "3":
-    result = math.multiplication(num1, nun2)
+    result = math.multiplication(num1, num2)
     print(f"ur result: ", result)
+elif option == "4":
+    if num2 == 0:
+        print("sorry cannot divide by zero :)")
+    else:
+        result = math.division(num1, num2)
+        print(f"ur result: ", result)
+elif option == "5":
+    print("Square root only support one number")
+    num1 = input("Enter a num: ")
+    num1 = int(num1)
+    result = math.square(num1)
+
+    print(f"ur result: ", result)
+else:
+    print("What you enterd pls enter 1-5. 😜")
--- math.py
+++ math.py
@@ -1,2 +1,10 @@
 def plus(num1, num2):
     return num1 + num2
+def minus(num1, num2):
+    return num1 - num2
+def multiplication(num1, num2):
+    return num1 * num2
+def division(num1, num2):
+    return num1 / num2
+def square(num1):
+    result = num1 ** 0.5
